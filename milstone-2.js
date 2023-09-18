5d395aef2b27148aa920703f86c8051ed7545f4f
Md Jabed khan <mdjabedkhan1106@gmail.com>
2023-09-18 23:50:20 +0600

    first commit

---
+++ milstone-2.js
@@ -0,0 +1,70 @@
+// 11. Write a program with javascript that can truncate a variable to a maximum length of 20 characters.
+
+// let project = "I want to build a multivendor website where user can buy and sell their products." ;
+// let finalProject = project.slice(0, 20);
+
+// console.log(finalProject)
+
+
+// 14. Declare variables considering a student first name, last name, age and result. And print all of this  within given format: 
+// Name: John Doe,
+// Age: 23,
+// GPA: 4.75,
+
+
+// let firstName = "Jabed";
+// let lastName = "khan";
+// let age = 21;
+// let result = 4.55;
+
+// let userInfo = `Name: ${firstName.concat(" " ,lastName)}
+// Age: ${age}
+// Result: ${result}`
+
+// console.log(userInfo)
+
+// 15. Write a program that takes a user-entered string containing a numeric float value and converts it to an integer.
+
+// let number = 20.656;
+// let answer= number.toFixed(0);
+
+// console.log(answer)
+
+
+// 16. Write a program that validates user input to ensure it's a valid number.
+
+// let number = 9990 ;
+// let final = isNaN(number)
+
+// console.log(final)
+
+
+// 17. Create a program that prompts the user for a string containing a decimal number and converts it to a floating-point number.
+
+
+
+// 18. Write a JavaScript program that can check if a string contains the given value. 
+// 		Example:
+// 			var myText = ‘The quick brown fox has jumped’
+// 			var myInput = ‘Fox’
+// 			Now check if myText variable contains myInput or not.
+
+// var myText = "The quick brown fox has jumped"
+// var myInput = myText.includes("Fox")
+
+// console.log(myInput)
+
+
+
+// 19. Imagine you have an employee ID of 8 digits. The first employee ID is 00000001. If you can declare a variable with the number value: 199 how can you make it total 8 digits by adding an additional 0 at the beginning? Think and try to use string methods.
+
+
+let text = "199";
+let padded = text.padStart(8,"0");
+
+console.log(padded)
+
+
+
+// 20. Develop a JavaScript program that generates an email template. The template should include placeholders for the recipient's name, sender's name, and a custom message.
+
